81df6472b13a6bc6bb32a33bf131db8732eba8a9
tetsu5555 <tetete1118@gmail.com>
2019-09-02 03:12:57 +0900

    feat: add webpack.confg.js

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // エントリーポイント
+    entry: './src/index.js',
+    // 出力設定
+    output: {
+        // 出力先のフォルダー
+        // ${__dirname}はプロジェクトフォルダーのパスを表す
+        path: `${__dirname}/dist`,
+        // 出力先のファイル名
+        filename: 'main.js'
+    }
+};
