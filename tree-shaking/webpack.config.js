3b61f8d5308d1205cc8ece6a2563e82b92f32de6
tetsu5555 <tetete1118@gmail.com>
2020-01-12 18:51:06 +0900

    test babel tree shaking

--- tree-shaking/webpack.config.js
+++ tree-shaking/webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index',
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'production'
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  mode: 'production',
 };
