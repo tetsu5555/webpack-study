a530576832682c668d56502945ff6c5e589c6779
tetsu5555 <tetete1118@gmail.com>
2020-01-12 18:31:16 +0900

    webpack

---
+++ tree-shaking/src/index.js
@@ -0,0 +1,3 @@
+import { moduleB } from './modules';
+
+moduleB();
---
+++ tree-shaking/src/modules.js
@@ -0,0 +1,9 @@
+const moduleA = () => {
+    console.log('this is moduleA');
+};
+
+const moduleB = () => {
+    console.log('this is moduleB');
+};
+
+export { moduleA, moduleB };
