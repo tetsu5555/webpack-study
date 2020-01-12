69bfc0e8230638c8ca8f2ed40ffa154df7c7a85b
tetsu5555 <tetete1118@gmail.com>
2020-01-12 19:14:55 +0900

    reduce bundle size with babel-plugin-transform-import

--- tree-shaking/src/index.js
+++ tree-shaking/src/index.js
@@ -1,5 +1,26 @@
-import { map } from 'lodash-es';
+// import map from 'lodash/map';
+// import differenceWith from 'lodash/differenceWith';
+// import divide from 'lodash/divide';
+// import drop from 'lodash/drop';
+// import dropRight from 'lodash/dropRight';
+// import dropRightWhile from 'lodash/dropRightWhile';
+
+// babel-plugin-transform-imports利用するとimportを簡潔にかける
+// ↑のようにファイルを個別で指定してimportしなくても良い
+import {
+  map,
+  differenceWith,
+  divide,
+  drop,
+  dropRight,
+  dropRightWhile,
+} from 'lodash';
 import { moduleB } from './modules';
 
-moduleB();
 map();
+differenceWith();
+divide();
+drop();
+dropRight();
+dropRightWhile();
+moduleB();
